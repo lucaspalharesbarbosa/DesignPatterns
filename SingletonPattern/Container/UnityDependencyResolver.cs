13c798c792a74359e3db54ffa7487032bbee2ab0
Lucas Palhares <lucasp.b@hotmail.com>
2020-06-27 10:48:25 -0300

    - UnityDependecyResolver.

---
+++ SingletonPattern/Container/UnityDependencyResolver.cs
@@ -0,0 +1,26 @@
+﻿using SingletonPattern.Interfaces;
+using System;
+using System.Collections.Generic;
+using Unity;
+
+namespace SingletonPattern.Container {
+    public class UnityDependencyResolver {
+        private static readonly IUnityContainer _container;
+
+        static UnityDependencyResolver() {
+            _container = new UnityContainer();
+
+            IoC.Initialize(_container);
+        }
+
+        public void EnsureDependenciesRegistered() {
+            _container.RegisterType<IFileLoggerFactory, LazySingletonFileLoggerFactory>();
+        }
+
+        public IUnityContainer Container {
+            get {
+                return _container;
+            }
+        }
+    }
+}
