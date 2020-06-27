cdce927ca72d026a19ab50045c8b8504ca9fd351
Lucas Palhares <lucasp.b@hotmail.com>
2020-06-27 17:48:37 -0300

    - Implementado padrão Prototype.

---
+++ PrototypePattern/DocumentReader.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace PrototypePattern {
+    public class DocumentReader : ICloneable {
+        private string _pageTitle;
+        private int _headerCount;
+        private string _bodyContent;
+
+        public DocumentReader(Uri uri) {
+            var httpClient = new HttpClient();
+
+            Task<string> contents = httpClient.GetStringAsync(uri);
+            string stringContents = contents.Result;
+
+            Analyse(stringContents);
+        }
+
+        private void Analyse(string stringContents) {
+            _pageTitle = "Homepage";
+            _headerCount = 2;
+            _bodyContent = "Welcome to my homepage";
+        }
+
+        public void PrintPageData() {
+            Console.WriteLine("Page title: {0}, header count: {1}, body: {2}", _pageTitle, _headerCount, _bodyContent);
+        }
+
+        public object Clone() {
+            return MemberwiseClone();
+        }
+    }
+}
---
+++ PrototypePattern/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PrototypePattern {
+    class Program {
+        static void Main(string[] args) {
+
+            var reader = new DocumentReader(new Uri("http://bbc.co.uk"));
+            reader.PrintPageData();
+
+            var readerClone = reader.Clone() as DocumentReader;
+            readerClone.PrintPageData();
+
+            Console.ReadKey();
+        }
+    }
+}
